24689b630c8b7207460a59d5ae66b67ce24acf07
Damiano Rocchi <Damiano.rocchi@gmail.com>
2022-06-15 17:41:57 +0200

    aggiunta interazione a layer per avere oggetti composti da cassa+specchio rifrazione aggiunta ma slightly broken(va in stackOverflowExecption)

--- Assets/Scripts/AmbientalElements/Laser/LaserBeam.cs
+++ Assets/Scripts/AmbientalElements/Laser/LaserBeam.cs
@@ -11,9 +11,17 @@ public class LaserBeam
 
     public GameObject laserObject;
     LineRenderer laserRenderer;
+    //se il raggio deve collidere con qualcosa basta aggiungerlo ai layer
+    int layer = LayerMask.GetMask("Mirror", "Player", "BlockLaser", "Refract");
 
     //lista per immagazzinare ogni punto del raggio laser
     List<Vector2> laserIndexes = new List<Vector2>();
+    //dizionario per eventuali differenti materiali per avere differenti rifrazioni
+    Dictionary<string, float> refractMaterials = new Dictionary<string, float>()
+    {
+        {"Air", 1f },
+        {"Glass", 1.5f }
+    };
 
     public LaserBeam(Vector2 pos, Vector2 dir, Material material, float range)
     {
@@ -42,7 +50,7 @@ public class LaserBeam
 
         //effettivo laser gestito da raycast
         Ray ray = new Ray(pos, dir);
-        RaycastHit2D hit = Physics2D.Raycast(pos, dir, laserRange);
+        RaycastHit2D hit = Physics2D.Raycast(pos, dir, laserRange, layer);
 
         if(hit.collider != null)
         {
@@ -58,7 +66,7 @@ public class LaserBeam
     private void CheckHit(RaycastHit2D hit, Vector2 direction, LineRenderer laser)
     {
         //se colpisce uno specchio il raggio viene deviato
-        if (hit.collider.gameObject.tag == "Mirror")
+        if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Mirror"))
         {
             Vector2 pos = hit.point;
             Vector2 dir = Vector2.Reflect(direction, hit.normal);
@@ -69,11 +77,57 @@ public class LaserBeam
             CastRay(newPos, dir, laser);
             //la funzione è ricorsiva, se serve mettere un limite basta aggiungere una variabile counter
         }
+        else if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Refract"))
+        {
+            //prendo il punto d'impatto
+            Vector3 pos = hit.point;
+            laserIndexes.Add(pos);
+
+            Vector3 newPos1 = new Vector3(pos.x + (0.01f * direction.x), pos.y + (0.01f * direction.y));
+
+            float n1 = refractMaterials["Air"];
+            //questo funziona solo se il nome del gameObject è lo stesso interno al dictionary
+            float n2 = refractMaterials[hit.collider.name];
+
+            Vector3 norm = hit.normal;
+            Vector3 incidentDir = direction;
+
+            Vector3 refractedVector = Refract(n1, n2, norm, incidentDir);
+
+            //una volta presa la rifrazione d'ingresso vado a calcolare quella d'uscita
+            Ray ray1 = new Ray(newPos1, refractedVector);
+            Vector3 newRayPos = ray1.GetPoint(1.5f);
+
+            Ray ray2 = new Ray(newRayPos, -refractedVector);
+            RaycastHit2D hit2 = Physics2D.Raycast(newRayPos, -refractedVector, 1.6f);
+
+            if(hit2)
+            {
+                laserIndexes.Add(hit2.point);
+            }
+
+            UpdateLaser();
+
+            Vector3 refractedVector2 = Refract(n2, n1, -hit2.normal, refractedVector);
+            CastRay(hit2.point, refractedVector2, laser);
+        }
         else
         {
             laserIndexes.Add(hit.point);
             UpdateLaser();
         }
+    }
+
+    //questo serve a calcolare la prima rifrazione
+    Vector3 Refract(float n1, float n2, Vector3 norm, Vector3 incident)
+    {
+        incident.Normalize();
+        //bro questa è fisica oltre il mio livello, non ho idea dei calcoli, la legge di Snell non l'ho mai studiata
+        Vector3 refractedVector = (n1 / n2 * Vector3.Cross(norm, Vector3.Cross(-norm, incident))
+            - norm * Mathf.Sqrt(1 - Vector3.Dot(Vector3.Cross(norm, incident) * (n1 / n2 * n1 / n2),
+            Vector3.Cross(norm, incident)))).normalized;
+
+        return refractedVector;
     }
 
     //updatelaser serve a far andare "in avanti" il laser
