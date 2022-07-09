eb119ee3035584633fc80bd5afa966c914596cc6
Alessandro Babini <babins95@yahoo.it>
2022-07-09 12:13:38 +0200

    Messo script per scalare telecamera a seconda dello schermo,tolto bug prima scena

---
+++ Assets/Scripts/CameraScale.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraScale : MonoBehaviour
+{
+    public int fullWidthUnits = 35;
+    void Start()
+    {
+        float ratio = (float)Screen.height / (float)Screen.width;
+        GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = (float)fullWidthUnits * ratio / 2.0f;
+    }
+}
